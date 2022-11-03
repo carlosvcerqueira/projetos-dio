e50d09b9e2056a5f24de5efb09a127300677a136
carlosvcerqueira <carlosvcerqueira@gmail.com>
2022-11-03 16:19:02 -0300

    aulas

---
+++ javascript/07 - importacao/main.js
@@ -0,0 +1,15 @@
+
+const {gets, print} = require('./funcoes-auxiliares')
+
+const numerosSorteados = [];
+let maiorValorEncontrado = 0;
+
+for (let i = 0; i < 5; i++) {
+    const numeroSorteado = gets();
+    if (numeroSorteado > maiorValorEncontrado) {
+        maiorValorEncontrado = numeroSorteado;
+    }
+}
+
+
+print(maiorValorEncontrado);
