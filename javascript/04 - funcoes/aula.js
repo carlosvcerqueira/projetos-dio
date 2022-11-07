5a9678e139ac1f2f2e365b422d161472af1104b5
carlosvcerqueira <carlosvcerqueira@gmail.com>
2022-11-07 13:50:10 -0300

    funcoes js

---
+++ javascript/04 - funcoes/aula.js
@@ -0,0 +1,22 @@
+// function myName(name) {
+//     console.log(`Seu nome é ${name}`)
+// }
+// myName('Carlos')
+// myName('Ana')
+
+// function quadrado(valor) {
+//     return valor * valor
+// }
+// const quadrado10 = quadrado(10);
+// console.log(quadrado10)
+
+// function incrementarJuros(valor, percentualJuros) {
+//     const valorDeAcrescimo = (percentualJuros / 100) * valor
+//     return valor + valorDeAcrescimo
+// }
+// console.log(incrementarJuros(100, 15))
+
+function main() {
+    console.log('Programa principal.')
+}
+main()
