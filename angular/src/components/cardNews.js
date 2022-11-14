95ca1007ffd0cf27896162555d6abfae50014b63
carlosvcerqueira <carlosvcerqueira@gmail.com>
2022-11-14 17:14:52 -0300

    angular

---
+++ angular/src/components/cardNews.js
@@ -0,0 +1,18 @@
+class Cardnews extends HTMLElement {
+    constructor(){
+        super();
+
+        const shadow = this.attachShadow({mode: "open"});
+        shadow.innerHTML = "<h1>Hello</h1>"
+    }
+
+    build() {
+
+    }
+
+    styles() {
+        
+    }
+}
+
+customElements.define("card-news", Cardnews);
